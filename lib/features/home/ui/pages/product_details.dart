417b3710e3c4d35d374d211d9b6e2c2cb6a99c90
mohamed_ashraf <moashrafasd12@gmail.com>
2025-05-28 16:57:35 +0300

    refactor product details design

--- lib/features/home/ui/pages/product_details.dart
+++ lib/features/home/ui/pages/product_details.dart
@@ -5,6 +5,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/theming/app_colors.dart';
 import '../../../../core/theming/app_text_styles.dart';
+import '../widgets/quantity_product_widget.dart';
 
 class ProductDetailsPage extends StatelessWidget {
   const ProductDetailsPage({super.key});
@@ -12,109 +13,133 @@ class ProductDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        Stack(
+      body: SingleChildScrollView(
+        child: Column(
           children: [
+            // Top Image Stack
+            Stack(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 350.h,
+                  decoration: const BoxDecoration(
+                    color: AppColors.primary,
+                    image: DecorationImage(
+                      image: AssetImage(AppAssets.offerImage),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 36.h, horizontal: 16.w),
+                  child: Icon(
+                    size: 30.sp,
+                    Icons.arrow_back,
+                    color: Colors.black87,
+                  ),
+                )
+              ],
+            ),
+
+            // Product Details Section
             Container(
               width: double.infinity,
-              height: 350.h,
-              decoration: const BoxDecoration(
-                  color: AppColors.primary,
-                  image: DecorationImage(
-                      image: AssetImage(AppAssets.offerImage),
-                      fit: BoxFit.cover)),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 36.h, horizontal: 16.w),
-              child: Icon(
-                size: 30.sp,
-                Icons.arrow_back,
-                color: Colors.black87,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.r),
+                color: AppColors.background,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          '\$ 2.22',
+                          style: AppTextStyles.font18PrimaryDarkSemiBoldCairo,
+                        ),
+                        const Spacer(),
+                        Padding(
+                          padding: EdgeInsets.only(right: 8.w, top: 4.h),
+                          child: Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                            size: 24.sp,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 8.h),
+                    Text(
+                      'Product Name',
+                      style: AppTextStyles.font20BlackSemiBoldCairo,
+                    ),
+                    Text(
+                      '1.50 lbs',
+                      style: AppTextStyles.font12GrayCairoRegular,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          '4.5',
+                          style: AppTextStyles.font16BlackCairoMedium,
+                        ),
+                        SizedBox(width: 8.w),
+                        SizedBox(
+                          width: 95.w,
+                          height: 20.h,
+                          child: ListView(
+                            scrollDirection: Axis.horizontal,
+                            physics: const NeverScrollableScrollPhysics(),
+                            children: List.generate(
+                              5,
+                              (index) => Icon(
+                                Icons.star,
+                                color:
+                                    index < 4 ? Colors.amber : AppColors.gray,
+                                size: 18.sp,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text(
+                          '(89 reviews)',
+                          style: AppTextStyles.font12GrayCairoRegular,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 16.h),
+                    Text(
+                      'Organic Mountain works as a seller for many organic growers of organic lemons. Organic lemons are easy to spot in your produce aisle. They are just like regular lemons, but they will usually have a few more scars on the outside of the lemon skin. Organic lemons are considered to be the world',
+                      style: AppTextStyles.font12GrayCairoRegular,
+                    ),
+                    SizedBox(height: 16.h),
+
+                    // Quantity Selector
+                    QuantityProductWidget(),
+
+                    SizedBox(height: 16.h),
+                  ],
+                ),
               ),
             )
           ],
         ),
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8.r),
-            color: AppColors.background,
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      '\$ 2.22',
-                      style: AppTextStyles.font18PrimaryDarkSemiBoldCairo,
-                    ),
-                    const Spacer(),
-                    Padding(
-                      padding: EdgeInsets.only(right: 8.w, top: 4.h),
-                      child: Icon(
-                        Icons.favorite,
-                        color: Colors.red,
-                        size: 24.sp,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 8.h),
-                Text(
-                  'Product Name',
-                  style: AppTextStyles.font20BlackSemiBoldCairo,
-                ),
-                Text(
-                  '1.50 lbs',
-                  style: AppTextStyles.font12GrayCairoRegular,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      '4.5',
-                      style: AppTextStyles.font16BlackCairoMedium,
-                    ),
-                    SizedBox(width: 8.w),
-                    SizedBox(
-                      width: 95.w, // Adjust width based on your layout
-                      height: 20.h,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        physics: const NeverScrollableScrollPhysics(),
-                        children: List.generate(
-                          5,
-                          (index) => Icon(
-                            Icons.star,
-                            color: index < 4 ? Colors.amber : AppColors.gray,
-                            size: 18.sp,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Text(
-                      '(89 reviews)',
-                      style: AppTextStyles.font12GrayCairoRegular,
-                    ),
-                  ],
-                ),
-                SizedBox(height: 16.h),
-                Text(
-                  'Organic Mountain works as a seller for many organic growers of organic lemons. Organic lemons are easy to spot in your produce aisle. They are just like regular lemons, but they will usually have a few more scars on the outside of the lemon skin. Organic lemons are considered to be the world',
-                  style: AppTextStyles.font12GrayCairoRegular,
-                ),
-                SizedBox(height: 16.h),
-                AppTextButton(
-                  buttonText: 'Add to Cart',
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ),
-        )
-      ],
-    ));
+      ),
+
+      // Sticky Bottom Button
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.all(16.h),
+        child: AppTextButton(
+          buttonText: 'Add to Cart',
+          image: AppAssets.cart,
+          onPressed: () {
+            // Your action
+          },
+        ),
+      ),
+    );
   }
 }
---
+++ lib/features/home/ui/widgets/quantity_product_widget.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class QuantityProductWidget extends StatefulWidget {
+  const QuantityProductWidget({super.key});
+
+  @override
+  State<QuantityProductWidget> createState() => _QuantityProductWidgetState();
+}
+
+class _QuantityProductWidgetState extends State<QuantityProductWidget> {
+  int quantity = 3;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 4.h),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8.r),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Quantity',
+            style: TextStyle(
+              fontSize: 16.sp,
+              color: Colors.grey,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const Spacer(),
+          IconButton(
+            onPressed: () {
+              if (quantity > 1) {
+                setState(() {
+                  quantity--;
+                });
+              }
+            },
+            icon: Icon(Icons.remove, color: Colors.green),
+            constraints: BoxConstraints(),
+            padding: EdgeInsets.all(8.w),
+          ),
+          Container(
+            alignment: Alignment.center,
+            width: 40.w,
+            decoration: BoxDecoration(
+              border: Border.symmetric(
+                vertical: BorderSide(color: Colors.grey.shade300),
+              ),
+            ),
+            child: Text(
+              '$quantity',
+              style: TextStyle(
+                fontSize: 16.sp,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                quantity++;
+              });
+            },
+            icon: Icon(Icons.add, color: Colors.green),
+            constraints: BoxConstraints(),
+            padding: EdgeInsets.all(8.w),
+          ),
+        ],
+      ),
+    );
+  }
+}
