f71198605e574119e51fa36d5f484f88e88f2214
mohamed_ashraf <moashrafasd12@gmail.com>
2025-06-04 19:04:33 +0300

    first

--- lib/features/sign_up/ui/pages/signup_page.dart
+++ lib/features/sign_up/ui/pages/signup_page.dart
@@ -13,6 +13,7 @@ import '../../../login/ui/widgets/shadow_appBar.dart';
 class SignupPage extends StatelessWidget {
   const SignupPage({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
