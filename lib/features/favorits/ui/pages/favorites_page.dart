f09451f67bf360eac701156afd00d5c46276e028
mohamed_ashraf <moashrafasd12@gmail.com>
2025-06-26 03:29:01 +0300

    update

--- lib/features/favorits/ui/pages/favorites_page.dart
+++ lib/features/favorits/ui/pages/favorites_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
+import '../../../../core/theming/app_colors.dart';
 import '../../../../core/theming/app_text_styles.dart';
 
 class FavoritesPage extends StatefulWidget {
@@ -24,64 +25,89 @@ class _FavoritesPageState extends State<FavoritesPage>
       ),
       body: ListView(
         children: [
-          Slidable(
-            key: const ValueKey(0),
-            // The end action pane is the one at the right or the bottom side.
-            endActionPane: ActionPane(
-              motion: const ScrollMotion(),
-              dismissible: DismissiblePane(onDismissed: () {}),
-              children: [
-                SlidableAction(
+          Padding(
+            padding:EdgeInsets.symmetric(horizontal: 10.w , vertical: 16.h),
+            child: Slidable(
+              key: const ValueKey(0),
+              endActionPane: ActionPane(
+                motion: const ScrollMotion(),
 
-                  flex: 1,
-                  onPressed: (_) => controller.openEndActionPane(),
-                  backgroundColor: Colors.red,
-                  foregroundColor: Colors.white,
-                  icon: Icons.delete,
-                  label: 'delete',
-                ),
-              ],
-            ),
-            child: Container(
-              padding:  EdgeInsets.symmetric(horizontal: 10.w , vertical: 16.h),
-              margin: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Row(
+                dismissible: DismissiblePane(onDismissed: () {
+                }),
                 children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: Image.asset(
-                      AppAssets.offerImage,
-                      height: 100.h,
-                      width: 100.w,
-                      fit: BoxFit.cover,
-                    )
+                  SlidableAction(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(8.r),
+                      bottomLeft: Radius.circular(8.r),
+                    ),
+                    onPressed: (_) => controller.openEndActionPane(),
+                    backgroundColor: Colors.deepOrange,
+                    foregroundColor: Colors.white,
+                    icon: Icons.delete,
+                    label: 'Delete',
+                  ),
+                ],
+              ),
+              child: Container(
+                padding:  EdgeInsets.symmetric(horizontal: 10.w , vertical: 16.h),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
                 ),
-                  SizedBox(width: 8.w,),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '\$ 100 * 4',
-                        style: AppTextStyles.font12GreenCairoMedium,
-                      ),
-                      Text(
-                        'Product name',
-                        style: AppTextStyles.font16BlackCairoMedium,
-                      ),
-                      Text(
-                        'Product description',
-                        style: AppTextStyles.font12GrayCairoRegular,
-                      ),
-                    ]
-                  )
-                ]
-              )
+                child: Row(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.asset(
+                        AppAssets.offerImage,
+                        height: 100.h,
+                        width: 100.w,
+                        fit: BoxFit.cover,
+                      )
+                  ),
+                    SizedBox(width: 8.w,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '\$ 100 * 4',
+                          style: AppTextStyles.font12GreenCairoMedium,
+                        ),
+                        SizedBox(height: 4.h,),
+                        Text(
+                          'Product name',
+                          style: AppTextStyles.font16BlackCairoMedium,
+                        ),
+                        SizedBox(height: 4.h,),
+                        Text(
+                          'Product description ',
+                          overflow: TextOverflow.ellipsis,
+                          style: AppTextStyles.font12GrayCairoRegular,
+                          maxLines: 2,
+                        ),
+                      ]
+                    ),
 
-            )
+                    Spacer(),
+                    Column(
+                      children: [
+                        Icon(Icons.add,color: AppColors.primaryDark,),
+                        SizedBox(height: 8.h,),
+
+                        Text(
+                          '5',
+                          style: AppTextStyles.font16BlackCairoMedium,
+                        ),
+                        SizedBox(height: 8.h,),
+                        Icon(Icons.remove,color: AppColors.primaryDark,)
+
+                      ],
+                    )
+                  ]
+                )
+
+              )
+            ),
           ),
         ],
       ),
@@ -89,4 +115,4 @@ class _FavoritesPageState extends State<FavoritesPage>
   }
 }
 
-void doNothing(BuildContext context) {}
+// void doNothing(BuildContext context) {}
